598491d719fa465b29d6b1e7942b6e50db71825d
ShaiHulud <gmpohta@google.com>
2023-06-08 14:04:42 +0800

    progress

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -17,8 +17,9 @@ class MainController extends AbstractController
     
     private EntityManagerInterface $entityManager;
 
-    public function __construct(EntityManagerInterface $em)
-    {
+    public function __construct(
+        EntityManagerInterface $em, 
+    ) {
         $this->em = $em;
     }
 
@@ -33,7 +34,14 @@ class MainController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $this->em->persist($article);
             $this->em->flush();
-            return $this->redirectToRoute('render_article', ['id' => $article->getId()]);
+
+            $headers = ['alg'=>'HS256','typ'=>'JWT'];
+            $payload = ['id'=>$article->getId(), 'exp'=>(time() + 3600*3)];
+            $this->addFlash('accessToken', $this->generateJwt($headers, $payload));
+
+            return $this->redirectToRoute('render_article', [
+                'id'=>$article->getId()
+            ]);
         }
 
         return $this->render('new-page.html.twig',[
@@ -41,9 +49,68 @@ class MainController extends AbstractController
         ]);
     }
 
+    #[Route('/edit/{id}', name:'edit_article')] 
+    public function editAction(int $id, Request $request)
+    {   
+        
+        if ($request->cookies->get('accessToken')) {
+            $decodedJWT = $this->isJwtValid($request->cookies->get('accessToken'), $secret = 'secret');        
+            $idFromToken = $decodedJWT['payload']['id'];
+            $isValid = $decodedJWT['isValid'];
+        }
+
+        if (
+            $request->cookies->get('accessToken') && 
+            $idFromToken == $id &&
+            $isValid
+        ) {
+            $article = $this->em
+                ->getRepository(Article::class)
+                ->findOneBy(['id' => $id]);
+
+            $form = $this->createForm(ArticleType::class, $article);
+            
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $this->em->persist($article);
+                $this->em->flush();
+
+                return $this->redirectToRoute('render_article', [
+                    'id'=>$article->getId()
+                ]);
+            } 
+
+            return $this->render('new-page.html.twig',[
+                'form' => $form->createView()
+            ]);
+        }
+        
+        return $this->redirectToRoute('create_new_article');
+    }
+
     #[Route('/{id}', name: 'render_article', requirements: ['id' => '\d+'])] 
-    public function renderAction(int $id)
+    public function renderAction(int $id, Request $request)
     {
+        $session = $request->getSession();
+        $accessToken = $session->getFlashBag()->get('accessToken');
+        
+        $mayEdit = false;
+
+        if ($request->cookies->get('accessToken')) {
+            $decodedJWT = $this->isJwtValid($request->cookies->get('accessToken'), $secret = 'secret');        
+            $idFromToken = $decodedJWT['payload']['id'];
+            $isValid = $decodedJWT['isValid'];
+        }
+
+        if (
+            $request->cookies->get('accessToken') && 
+            $idFromToken == $id &&
+            $isValid
+        ) {
+            $mayEdit = true;
+        }
+
         $article = $this->em
             ->getRepository(Article::class)
             ->findOneBy(['id' => $id]);
@@ -52,15 +119,14 @@ class MainController extends AbstractController
             throw $this->createNotFoundException(sprintf('Article with id "%s" not found.', $id));
         }
 
-        $mayEdit = false;//
-
         return $this->render('page.html.twig', [
             'article' => $article,
-            'mayEdit' => $mayEdit
+            'mayEdit' => $mayEdit,
+            'accessToken' => $accessToken ? $accessToken[0] : null,
         ]);
     }
 
-     #[Route('/upload', name:'upload_file')]
+    #[Route('/upload', name:'upload_file')]
     public function uploadFileAction(Request $request): JsonResponse
     {
         $files = array_values($request->files->all());
@@ -80,5 +146,45 @@ class MainController extends AbstractController
         return new JsonResponse(
             $fileToUpload->getWebPath(), 200
         );
+    }
+
+    function generateJwt($headers, $payload, $secret = 'secret') {
+        $headers_encoded = base64_encode(json_encode($headers));
+        
+        $payload_encoded = base64_encode(json_encode($payload));
+        
+        $signature = hash_hmac('SHA256', "$headers_encoded.$payload_encoded", $secret, true);
+        $signature_encoded = base64_encode($signature);
+        
+        $jwt = "$headers_encoded.$payload_encoded.$signature_encoded";
+        
+        return $jwt;
+    }
+
+    function isJwtValid($jwt, $secret = 'secret') {
+        // split the jwt
+        $tokenParts = explode('.', $jwt);
+        $header = base64_decode($tokenParts[0]);
+        $payload = base64_decode($tokenParts[1]);
+        $signature_provided = $tokenParts[2];
+    
+        // check the expiration time - note this will cause an error if there is no 'exp' claim in the jwt
+        $expiration = json_decode($payload)->exp;
+        $is_token_expired = ($expiration - time()) < 0;
+    
+        // build a signature based on the header and payload using the secret
+        $base64_url_header = base64_encode($header);
+        $base64_url_payload = base64_encode($payload);
+        $signature = hash_hmac('SHA256', $base64_url_header . "." . $base64_url_payload, $secret, true);
+        $base64_url_signature = base64_encode($signature);
+    
+        // verify it matches the signature provided in the jwt
+        $is_signature_valid = ($base64_url_signature === $signature_provided);
+    
+        if ($is_token_expired || !$is_signature_valid) {
+            return ['isValid' => FALSE];
+        } else {
+            return ['isValid' => TRUE, 'payload' => json_decode($payload, TRUE)];
+        }
     }
 }
